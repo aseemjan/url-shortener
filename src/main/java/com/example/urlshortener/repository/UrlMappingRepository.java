047ae410f31128cb19b0bb9c307ec02e364d2ab7
Aseem <aseemjan7@gmail.com>
2025-09-30 11:16:27 +0530

    Removed a redundant (identical) line

--- src/main/java/com/example/urlshortener/repository/UrlMappingRepository.java
+++ src/main/java/com/example/urlshortener/repository/UrlMappingRepository.java
@@ -2,7 +2,6 @@ package com.example.urlshortener.repository;
 
 import com.example.urlshortener.model.UrlMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.urlshortener.model.UrlMapping;
 import java.util.Optional;
 
 public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
